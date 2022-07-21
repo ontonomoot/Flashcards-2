0d1099e6b60c4d6451fa1ea0d50dafd80e81150f
Marie E <marrrielle.e@gmail.com>
2022-07-21 18:00:02 +0300

    создала базу данных

---
+++ db/migrations/20220721144610-create-users.js
@@ -0,0 +1,29 @@
+module.exports = {
+  async up(queryInterface, DataTypes) {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      name: {
+        type: DataTypes.TEXT,
+      },
+      totalScore: {
+        type: DataTypes.INTEGER,
+      },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
+    });
+  },
+  async down(queryInterface) {
+    await queryInterface.dropTable('Users');
+  },
+};
