3e187ec6bf1333d938714c9882f42f0c52010bd6
Marie E <marrrielle.e@gmail.com>
2022-07-22 16:17:10 +0300

    Release 3

--- routes/home.routes.js
+++ routes/home.routes.js
@@ -6,20 +6,20 @@ const homeRoutes = require('express').Router();
 const Hello = require('../views/Hello');
 const Themes = require('../views/Themes');
 const Chosen = require('../views/Chosen');
-const { Categories } = require('../db/models/categories');
+const { Categories } = require('../db/models');
 
 homeRoutes.get('/', (req, res) => {
   res.renderComponent(Hello, { title: 'Бобро Пожаловать!', greetings: 'Как твоё имя, умник?' });
 });
 
 homeRoutes.post('/categories', async (req, res) => {
-  // const user = req.body.player;
-  const themes = await Categories.findAll();
-  console.log(themes);
   const user = req.body.player;
+  const themes = await Categories.findAll({ raw: true });
+  const list = themes.map((el) => el.title)
+  // console.log(list);
 
   const main = React.createElement(Themes, {
-    title: `Твой ответ, ${user}?`, greetings: 'ТЕМЫ ДНЯ: ', text1: 'Разгрузиться:', text2: 'По фильмам', text3: 'Детские',
+    title: `Твой ответ, ${user}?`, greetings: 'ТЕМЫ ДНЯ: ', text1: `${list[0]}`, text2: `${list[1]}`, text3: `${list[2]}`,
   });
   const html = ReactDOMServer.renderToStaticMarkup(main);
   res.write('<!DOCTYPE html>');
--- views/Themes.jsx
+++ views/Themes.jsx
@@ -10,17 +10,17 @@ module.exports = function Themes({
       <p>
         {greetings}
       </p>
-      <dev>{text1}</dev>
+      {/* <dev>{text1}</dev> */}
       <form action="/input1" method="post">
-        <button type="submit">Выбрать</button>
+        <button type="submit">{text1}</button>
       </form>
-      <dev>{text2}</dev>
+      {/* <dev>{text2}</dev> */}
       <form action="/input2" method="post">
-        <button type="submit">Выбрать</button>
+        <button type="submit">{text2}</button>
       </form>
-      <dev>{text3}</dev>
+      {/* <dev>{text3}</dev> */}
       <form action="/input3" method="post">
-        <button type="submit">Выбрать</button>
+        <button type="submit">{text3}</button>
       </form>
     </Layout>
   );
