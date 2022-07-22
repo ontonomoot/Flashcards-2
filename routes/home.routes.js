b4c7f23216050cc01fb211eca0728a332a64001d
Anton Gorozhankin <obrv.igl@gmail.com>
2022-07-22 13:36:29 +0300

    Добавлена страница с категориями

--- routes/home.routes.js
+++ routes/home.routes.js
@@ -15,9 +15,34 @@ homeRoutes.get('/', (req, res) => {
 
 homeRoutes.post('/categories', (req, res) => {
   const user = req.body.player;
-  const main = React.createElement(Categories, { title: 'Categories', greetings: `Your version, ${user}?`, category: ' text1 text2 text3' });
+  const main = React.createElement(Categories, { title: `Твой ответ, ${user}?`, greetings: 'Вопросы дня: ', text1: '111', text2: '2222', text3: '3333' });
   const html = ReactDOMServer.renderToStaticMarkup(main);
   res.write('<!DOCTYPE html>');
   res.end(html);
 });
+homeRoutes.post('/categories/input1', (req, res) => {
+  const user = req.body.player;
+  const main = React.createElement(Categories, { title: `Твой ответ, ${user}?`, greetings: 'Вопросы дня: ', text1: '111', text2: '2222', text3: '3333' });
+  const html = ReactDOMServer.renderToStaticMarkup(main);
+  res.write('<!DOCTYPE html>');
+  res.end(html);
+});
+
+homeRoutes.post('/categories/input2', (req, res) => {
+  const user = req.body.player;
+  const main = React.createElement(Categories, { title: `Твой ответ, ${user}?`, greetings: 'Вопросы дня: ', text1: '111', text2: '2222', text3: '3333' });
+  const html = ReactDOMServer.renderToStaticMarkup(main);
+  res.write('<!DOCTYPE html>');
+  res.end(html);
+});
+
+homeRoutes.post('/categories/input3', (req, res) => {
+  const user = req.body.player;
+  const main = React.createElement(Categories, { title: `Твой ответ, ${user}?`, greetings: 'Вопросы дня: ', text1: '111', text2: '2222', text3: '3333' });
+  const html = ReactDOMServer.renderToStaticMarkup(main);
+  res.write('<!DOCTYPE html>');
+  res.end(html);
+});
+
+
 module.exports = homeRoutes;
